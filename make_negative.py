8852136e098085ed78da4f29038c87f6b7457bc8
Vedant <vuprajapati20@gmail.com>
2024-09-18 13:11:28 +0530

    math function can be really handy instead of doing manual labour

---
+++ make_negative.py
@@ -0,0 +1,19 @@
+"""
+My thinking process involved 
+substracting the number from its twice value E.g 2 - (2*2) = -2
+I didn't know we can directly return -number
+"""
+def make_negative(number):
+    if number <=0 :
+        return number
+    return eval('{}{}{}'.format(number, '-', number*2))
+
+"""
+Best One Liner Solution
+"""
+# def make_negative(number):
+#     return -abs(number)
+
+number = [1, 10, -98, 0, -1]
+for i in number:
+    print(make_negative(i))
